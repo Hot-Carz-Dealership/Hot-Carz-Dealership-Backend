0ae2653cf74fabb4af969a2bb2e8e92f1c26a70f
Daniel Santos Martinez <danielsm535@gmail.com>
2024-04-04 23:45:24 -0400

    Added documentation files for swaggery for the basic backend operation

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_swagger_ui import get_swaggerui_blueprint
 from config import Config  # Import the configuration
 
-
-
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/docs.yml'  # Our API url (can of course be a local resource)
 
 app = Flask(__name__)
 
@@ -20,6 +21,17 @@ app.config.from_object(Config)
 
 # Enable Cross-Origin Resource Sharing (CORS)
 CORS(app, supports_credentials=True)
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 # Initialize SQLAlchemy
 db = SQLAlchemy(app)
