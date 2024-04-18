274acd7362c99481b9375c2330d4c7e8325f5bb8
Daniel Santos Martinez <danielsm535@gmail.com>
2024-04-17 20:11:01 -0400

    Added new changes of DB into DB scripts in here

--- sql-database-scripts/db_scripts/db-schema.sql
+++ sql-database-scripts/db_scripts/db-schema.sql
@@ -28,8 +28,11 @@ CREATE TABLE IF NOT EXISTS CarVINs (
     # needed to differentiate between the car brought into the service center being bought from outside ot inside the dealership
     itemID INT AUTO_INCREMENT PRIMARY KEY,
     VIN_carID VARCHAR(17) UNIQUE,
-    purchase_status ENUM ('Dealership', 'Outside Dealership')
+    purchase_status ENUM ('Dealership - Not Purchased', 'Dealership - Purchased', 'Outside Dealership')
 );
+-- will have to change endpoints to ensure that carVins are valid in purchases and such with key relationships if their
+-- purchase status is 'Dealership - Purchased'
+-- also endpoint that changes the status here in the db of the car purchase status as well
 
 CREATE TABLE IF NOT EXISTS CarInfo (  # prev. known as  Cars
     # this table is meant to serve the purpose of containing information on the cars stored in the dealership for sale
@@ -41,7 +44,7 @@ CREATE TABLE IF NOT EXISTS CarInfo (  # prev. known as  Cars
     year INT,
     color VARCHAR(50),
     mileage INT,
-    details VARCHAR(255),
+    details TEXT,
     description TEXT,
     viewsOnPage INT,
     pictureLibraryLink TEXT,
@@ -58,6 +61,7 @@ CREATE TABLE IF NOT EXISTS Member (
     email VARCHAR(100),
     phone VARCHAR(20),
     address VARCHAR(255),
+    city VARCHAR(20),
     state VARCHAR(2),
     zipcode VARCHAR(5),
     join_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -82,6 +86,9 @@ CREATE TABLE IF NOT EXISTS Employee (
     email VARCHAR(100),
     phone VARCHAR(20),
     address VARCHAR(255),
+    city VARCHAR(20),
+    state VARCHAR(2),
+    zipcode VARCHAR(5),
     employeeType ENUM('superAdmin', 'Manager', 'Technician')
 );
 
@@ -122,30 +129,13 @@ CREATE TABLE IF NOT EXISTS ServiceAppointment (
     serviceID INT,
     appointment_date TIMESTAMP,
     comments TEXT,
-    status ENUM ('Scheduled', 'Done'),
+    status ENUM ('Scheduled', 'Done', 'Cancelled'),
     last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (memberID) REFERENCES Member(memberID),
     FOREIGN KEY (serviceID) REFERENCES Services(serviceID),
     FOREIGN KEY (VIN_carID) REFERENCES CarInfo(VIN_carID)
 );
 
-# CREATE TABLE IF NOT EXISTS ServiceAppointment (
-#     -- this table is meant to store information on the service appointments made
-#     appointment_id INT AUTO_INCREMENT PRIMARY KEY,
-#     memberID INT,
-# #     VIN_carID VARCHAR(17), -- prof clarify
-#     appointment_date TIMESTAMP,
-#     service_name VARCHAR(100),
-#     comments TEXT,
-#     status ENUM ('Scheduled', 'Done'),
-#     last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-#     FOREIGN KEY (memberID) REFERENCES Member(memberID)
-# #     FOREIGN KEY (service_name) REFERENCES Services(service_name)
-# #     FOREIGN KEY (memberID) REFERENCES Purchases(memberID)
-# #     FOREIGN KEY (VIN_carID) REFERENCES Purchases(VIN_carID) -- prof clarify
-# );
-
--- make fake data for this one
 CREATE TABLE IF NOT EXISTS ServiceAppointmentEmployeeAssignments (
     assignmentID INT AUTO_INCREMENT PRIMARY KEY,
     appointment_id INT,
@@ -171,14 +161,14 @@ CREATE TABLE IF NOT EXISTS Financing (
 CREATE TABLE IF NOT EXISTS Payments (
     paymentID INT AUTO_INCREMENT PRIMARY KEY,
     paymentStatus ENUM('Completed', 'Pending', 'Failed', 'None'),
-    valuePaid VARCHAR(20),
-    valueToPay VARCHAR(20),
+    valuePaid VARCHAR(20), # what you just paid
+    valueToPay VARCHAR(20), # the rest of what you still have to pay
     initialPurchase TIMESTAMP NULL DEFAULT NULL,
     lastPayment TIMESTAMP NULL DEFAULT NULL,
-    paymentType ENUM('Check/Bank Account', 'Card', 'None'),
-    cardNumber TEXT, -- Assuming card numbers are stored as strings
-    expirationDate TEXT, -- Storing as MM/YY format
-    CVV TEXT, -- Assuming CVV is a string with a fixed length
+--  paymentType ENUM('Check/Bank Account', 'Card', 'None'),
+--     cardNumber TEXT, -- Assuming card numbers are stored as strings
+--    expirationDate TEXT, -- Storing as MM/YY format
+--    CVV TEXT, -- Assuming CVV is a string with a fixed length
     routingNumber TEXT, -- Assuming routing number is stored as string
     bankAcctNumber TEXT, -- Assuming bank account number is stored as string
     memberID INT,
@@ -204,8 +194,11 @@ CREATE TABLE IF NOT EXISTS Purchases (
     VIN_carID VARCHAR(17),
     memberID INT,
     confirmationNumber VARCHAR(13) UNIQUE,
+    purchaseType ENUM ('Vehicle/Add-on Purchase', 'Vehicle/Add-on Continuing Payment', 'Service Payment'),
+    purchaseDate TIMESTAMP,
+    signature ENUM ('Yes', 'No'),
     FOREIGN KEY (bidID) REFERENCES Bids(bidID),
-    FOREIGN KEY (VIN_carID) REFERENCES CarInfo(VIN_carID),
+    FOREIGN KEY (VIN_carID) REFERENCES CarVINs(VIN_carID),
     FOREIGN KEY (memberID) REFERENCES Member(memberID)
 );
 
@@ -215,209 +208,3 @@ CREATE TABLE IF NOT EXISTS Addons (
     itemName VARCHAR(100),
     totalCost DECIMAL(10, 2)
 );
-
-
--- don't worry about these tables yet, later when testing phase comes
--- Audit tables for tracking events
-
-# CREATE TABLE IF NOT EXISTS MemberAuditLog (
-#     logID INT AUTO_INCREMENT PRIMARY KEY,
-#     memberID INT,
-#     event_description TEXT,
-#     event_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-#     FOREIGN KEY (memberID) REFERENCES Member(memberID)
-# );
-#
-# CREATE TABLE IF NOT EXISTS EmployeeAuditLog (
-#     logID INT AUTO_INCREMENT PRIMARY KEY,
-#     employeeID INT,
-#     event_description TEXT,
-#     event_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-#     FOREIGN KEY (employeeID) REFERENCES Employee(employeeID)
-# );
-#
-#
-# -- Trigger to log member updates
-# DELIMITER //
-#
-# CREATE TRIGGER member_update_trigger
-# AFTER UPDATE ON Member
-# FOR EACH ROW
-# BEGIN
-#     DECLARE log_description VARCHAR(255);
-#
-#     -- Initialize the log description
-#     SET log_description = '';
-#
-#     -- Check if first name was updated
-#     IF OLD.first_name != NEW.first_name THEN
-#         SET log_description = CONCAT(log_description, 'First name updated ' , OLD.first_name, ' to ', NEW.first_name);
-#     END IF;
-#
-#     -- Check if last name was updated
-#     IF OLD.last_name != NEW.last_name THEN
-#         SET log_description = CONCAT(log_description, 'Last name updated ' , OLD.last_name, ' to ', NEW.last_name);
-#     END IF;
-#
-#     -- Check if email was updated
-#     IF OLD.email != NEW.email THEN
-#         SET log_description = CONCAT(log_description, 'Email updated ' , OLD.email, ' to ', NEW.email);
-#     END IF;
-#
-#     -- Check if phone number was updated
-#     IF OLD.phone != NEW.phone THEN
-#         SET log_description = CONCAT(log_description, 'Phone number updated' , OLD.phone, ' to ', NEW.phone);
-#     END IF;
-#
-#     -- Check if join date was updated
-#     IF OLD.join_date != NEW.join_date THEN
-#         SET log_description = CONCAT(log_description, 'Join date updated from ', OLD.join_date, ' to ', NEW.join_date, '; ');
-#     END IF;
-#
-#     -- Insert log entry if any fields were updated
-#     IF log_description != '' THEN
-#         INSERT INTO MemberAuditLog (memberID, event_description)
-#         VALUES (NEW.memberID, log_description);
-#     END IF;
-# END//
-#
-# DELIMITER ;
-#
-#
-# -- Trigger to log changes in MemberSensitiveInfo table
-# DELIMITER //
-#
-# CREATE TRIGGER member_sensitive_info_trigger
-# AFTER UPDATE ON MemberSensitiveInfo
-# FOR EACH ROW
-# BEGIN
-#     DECLARE log_description VARCHAR(255);
-#     DECLARE lastModified TIMESTAMP;
-#     SET lastModified = CURRENT_TIMESTAMP;
-#
-#     -- Initialize the log description
-#     SET log_description = '';
-#
-#     -- Check if SSN was updated
-#     IF OLD.SSN != NEW.SSN THEN
-#         SET log_description = CONCAT(log_description, 'SSN updated ', OLD.SSN, ' to ', NEW.SSN);
-#     END IF;
-#
-#     -- Check if username was updated
-#     IF OLD.username != NEW.username THEN
-#         SET log_description = CONCAT(log_description, 'Username updated ', OLD.username, ' to ', NEW.username);
-#     END IF;
-#
-#     -- Check if password was updated
-#     IF OLD.password != NEW.password THEN
-#         SET log_description = CONCAT(log_description, 'Password updated from ', OLD.password, ' to ', NEW.password);
-#     END IF;
-#
-#     -- Check if driverID was updated
-#     IF OLD.driverID != NEW.driverID THEN
-#         SET log_description = CONCAT(log_description, 'Driver ID updated ', OLD.driverID, ' to ', NEW.driverID);
-#     END IF;
-#
-#     -- Check if cardInfo was updated
-#     IF OLD.cardInfo != NEW.cardInfo THEN
-#         SET log_description = CONCAT(log_description, 'Card info updated ', OLD.cardInfo, ' to ', NEW.cardInfo);
-#     END IF;
-#
-#     -- Insert log entry if any fields were updated
-#     IF log_description != '' THEN
-#         INSERT INTO MemberAuditLog (memberID, event_description)
-#         VALUES (NEW.memberID, log_description);
-#     END IF;
-# END//
-#
-# DELIMITER ;
-#
-#
-# -- Trigger to log employee updates
-# DELIMITER //
-#
-# CREATE TRIGGER employee_update_trigger
-# AFTER UPDATE ON Employee
-# FOR EACH ROW
-# BEGIN
-#     DECLARE log_description VARCHAR(255);
-#
-#     -- Initialize the log description
-#     SET log_description = '';
-#
-#     -- Check if firstname was updated
-#     IF OLD.firstname != NEW.firstname THEN
-#         SET log_description = CONCAT(log_description, 'First name updated ' , OLD.firstname, ' to ', NEW.firstname);
-#     END IF;
-#
-#     -- Check if lastname was updated
-#     IF OLD.lastname != NEW.lastname THEN
-#         SET log_description = CONCAT(log_description, 'Last name updated ' , OLD.lastname, ' to ', NEW.lastname);
-#     END IF;
-#
-#     -- Check if email was updated
-#     IF OLD.email != NEW.email THEN
-#         SET log_description = CONCAT(log_description, 'Email updated ' , OLD.email, ' to ', NEW.email);
-#     END IF;
-#
-#     -- Check if phone number was updated
-#     IF OLD.phone != NEW.phone THEN
-#         SET log_description = CONCAT(log_description, 'Phone number updated' , OLD.phone, ' to ', NEW.phone);
-#     END IF;
-#
-#     -- Check if address was updated
-#     IF OLD.address != NEW.address THEN
-#         SET log_description = CONCAT(log_description, 'Address updated ', OLD.address, ' to ', NEW.address);
-#     END IF;
-#
-#     -- Check if employeeType was updated
-#     IF OLD.employeeType != NEW.employeeType THEN
-#         SET log_description = CONCAT(log_description, 'Employee type updated from ', OLD.employeeType, ' to ', NEW.employeeType);
-#     END IF;
-#
-#     -- Insert log entry if any fields were updated
-#     IF log_description != '' THEN
-#         INSERT INTO EmployeeAuditLog (employeeID, event_description)
-#         VALUES (NEW.employeeID, log_description);
-#     END IF;
-# END//
-#
-# DELIMITER ;
-#
-#
-# -- Trigger to log changes in EmployeeSensitiveInfo table
-# DELIMITER //
-#
-# CREATE TRIGGER employee_sensitive_info_trigger
-# AFTER UPDATE ON EmployeeSensitiveInfo
-# FOR EACH ROW
-# BEGIN
-#     DECLARE log_description VARCHAR(255);
-#     DECLARE lastModified TIMESTAMP;
-#     SET lastModified = CURRENT_TIMESTAMP;
-#
-#     -- Initialize the log description
-#     SET log_description = '';
-#
-#     -- Check if password was updated
-#     IF OLD.password != NEW.password THEN
-#         SET log_description = CONCAT(log_description, 'Password updated ');
-#     END IF;
-#
-#     -- Check if SSN was updated
-#     IF OLD.SSN != NEW.SSN THEN
-#         SET log_description = CONCAT(log_description, 'SSN updated ');
-#     END IF;
-#
-#     -- Check if driverID was updated
-#     IF OLD.driverID != NEW.driverID THEN
-#         SET log_description = CONCAT(log_description, 'Driver ID updated ');
-#     END IF;
-#
-#     -- Insert log entry if any sensitive fields were updated
-#     IF log_description != '' THEN
-#         INSERT INTO EmployeeAuditLog (employeeID, event_description)
-#         VALUES (NEW.employeeID, CONCAT('Employee sensitive info updated: ', log_description));
-#     END IF;
-# END//
-# DELIMITER ;
