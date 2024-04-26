818b7ce0b2770e093e87f66772fa0cb2755a725a
Daniel Santos Martinez <danielsm535@gmail.com>
2024-04-26 16:43:10 -0400

    more changes in db to add to the hosting site

--- config.py
+++ config.py
@@ -10,14 +10,20 @@ load_dotenv()
 class Config:
     # the issue is here
     # General configuration settings
-    # SECRET_KEY = os.getenv("SECRET_KEY_3")
-    # if os.getenv('FLASK_ENV') == 'testing':
-        # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{os.getenv('SECRET_KEY_3')}@localhost/dealership_testing"
-        # SECRET_KEY = os.getenv("SECRET_KEY_2")
-        # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{SECRET_KEY}@roundhouse.proxy.rlwy.net:59865/dealership_testing"
-    # else:
-        # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{os.getenv('SECRET_KEY_3')}@localhost/dealership_backend"
-        # SECRET_KEY = os.getenv("SECRET_KEY_1")
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:aGGeAzhlGdyhqpkesCDkjgcyKXHYXEuK@viaduct.proxy.rlwy.net:20836/dealership_backend"
+
+    # use SECRET_KEY only for local dev testing and usage
+    # SECRET_KEY = os.getenv("SECRET_KEY")
+    if os.getenv('FLASK_ENV') == 'testing':
+        # uncomment for local dev | comment for hosting
+        # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{os.getenv('SECRET_KEY')}@localhost/dealership_testing"
+
+        # comment for local dev
+        SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:DHUvhsiNUNQMwrfUIqNCsVKWPebQBevT@roundhouse.proxy.rlwy.net:59865/dealership_testing"
+    else:
+        # uncomment for local dev | comment for hosting
+        # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{os.getenv('SECRET_KEY')}@localhost/dealership_backend"
+
+        # comment for local dev | uncomment for hosting
+        SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:aGGeAzhlGdyhqpkesCDkjgcyKXHYXEuK@viaduct.proxy.rlwy.net:20836/dealership_backend"
     os.environ['FLASK_ENV'] = 'development'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
