a52275c41b401a7ff2cd58a8e07ee42b23e07f1e
Daniel Santos Martinez <danielsm535@gmail.com>
2024-04-26 14:23:00 -0400

    ugh

--- config.py
+++ config.py
@@ -10,14 +10,15 @@ load_dotenv()
 class Config:
     # the issue is here
     # General configuration settings
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    # SECRET_KEY = os.getenv("SECRET_KEY_3")
     if os.getenv('FLASK_ENV') == 'testing':
         # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{os.getenv('SECRET_KEY_3')}@localhost/dealership_testing"
-        SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{os.getenv('SECRET_KEY_2')}@roundhouse.proxy.rlwy.net:59865/dealership_testing"
-
+        SECRET_KEY = os.getenv("SECRET_KEY_2")
+        SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{SECRET_KEY}@roundhouse.proxy.rlwy.net:59865/dealership_testing"
     else:
         # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{os.getenv('SECRET_KEY_3')}@localhost/dealership_backend"
-        SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{os.getenv('SECRET_KEY_1')}@viaduct.proxy.rlwy.net:20836/dealership_backend"
+        SECRET_KEY = os.getenv("SECRET_KEY_1")
+        SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{SECRET_KEY}@viaduct.proxy.rlwy.net:20836/dealership_backend"
 
     os.environ['FLASK_ENV'] = 'development'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
