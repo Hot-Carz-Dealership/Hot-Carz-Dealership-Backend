213319770838e01491ebd67f52ff0d3beaf5c3fe
Daniel Santos Martinez <danielsm535@gmail.com>
2024-04-26 17:04:06 -0400

    test json fix

--- app/tests/test_backend_routes.py
+++ app/tests/test_backend_routes.py
@@ -525,6 +525,7 @@ def test_service_menu_edit(client):
     edit_service_data = {
         "edit_or_add": 2,
         "serviceID": 4,
+        "price": 100.00,
         "service_name": "Edit the Service Name TEST"
     }
     response = client.post('/api/manager/edit-service-menu', json=edit_service_data, headers={'employee_session_id': employee_session_id})
