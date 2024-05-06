65659e50b2e6bbe65a70185455c401de59e73918
Antonio Arce <antoarcejr@gmail.com>
2024-05-05 20:51:37 -0400

    moved manager/getgfinacing

--- app/routes.py
+++ app/routes.py
@@ -2264,51 +2264,6 @@ def pending_service_appointments():
         return jsonify({'message': str(e)}), 500
 
 
-#FOR MANAGER TO GET THE FINANCING INFO OF A SPECIFIC MEMBER
-@app.route('/api/manager/get-financing', methods=['POST'])
-def get_financing_for_member():
-    try:
-        # Get the member ID from the request JSON data
-        request_data = request.json
-        member_id = request_data.get('member_id')
-
-        # Validate the member ID
-        if not member_id:
-            return jsonify({'message': 'Member ID is required'}), 400
-
-        # Query financing information for the specified member
-        financing_info = Financing.query \
-            .filter_by(memberID=member_id) \
-            .all()
-
-        # Check if any financing information is found
-        if not financing_info:
-            return jsonify({'message': 'No financing information found for the member'}), 404
-
-        # Serialize the financing information
-        serialized_data = []
-        for financing in financing_info:
-            # Fetch member details using a separate query
-            member = Member.query.get(financing.memberID)
-            if member:
-                serialized_data.append({
-                    'VIN_carID': financing.VIN_carID,
-                    'income': financing.income,
-                    'credit_score': financing.credit_score,
-                    'loan_total': financing.loan_total,
-                    'down_payment': financing.down_payment,
-                    'percentage': financing.percentage,
-                    'monthly_payment_sum': financing.monthly_payment_sum,
-                    'remaining_months': financing.remaining_months,
-                    'first_name': member.first_name,
-                    'last_name': member.last_name,
-                    'phone': member.phone
-                })
-
-        return jsonify(serialized_data), 200
-    except Exception as e:
-        return jsonify({'message': f'Error: {str(e)}'}), 500
-
 #I THINK THIS WAS MADE ALREADY,BUT IF NOT, FOR MANAGER TO GET INFO ABOUT A MEMBER
 @app.route('/api/manager/get_member', methods=['POST'])
 def get_member_by_id():
@@ -2422,38 +2377,43 @@ def current_member_bids():
         
         
 
-# this should be on the finacial end point 
-@app.route('/api/vehicle-purchase/new-bid-insert', methods=['POST'])
-def bid_insert_no_financing():
-    try:
-        # Extract data from the request
-        data = request.get_json()
-        required_fields = ['member_id', 'vin', 'bid_value', 'bid_status']
-        
-        # Check if all required fields are present
-        missing_fields = [field for field in required_fields if field not in data]
-        if missing_fields:
-            return jsonify({'message': f'Error: Missing fields - {", ".join(missing_fields)}'}), 400
-        
-        # Extract data
-        member_id = data['member_id']
-        vin = data['vin']
-        bid_value = data['bid_value']
-        bid_status = 'Processing'
-        
-        # Create a new bid entry
-        new_bid = Bids(
-            memberID=member_id,
-            VIN_carID=vin,
-            bidValue=bid_value,
-            bidStatus=bid_status,
-            bidTimestamp=datetime.now()
-        )
+''' Im Moving all the financial end point back to the stub. '''
+''' Anything Below Here will now be in the financial stub and should be working'''
 
-        db.session.add(new_bid)
-        db.session.commit()
-        return jsonify({'message': 'Bid successfully inserted.'}), 201
-    except Exception as e:
-        # Rollback the transaction in case of an error
-        db.session.rollback()
-        return jsonify({'message': f'Error: {str(e)}'}), 500
+
+'''On the Fin Stub'''
+# # this should be on the finacial end point 
+# @app.route('/api/vehicle-purchase/new-bid-insert', methods=['POST'])
+# def bid_insert_no_financing():
+#     try:
+#         # Extract data from the request
+#         data = request.get_json()
+#         required_fields = ['member_id', 'vin', 'bid_value', 'bid_status']
+        
+#         # Check if all required fields are present
+#         missing_fields = [field for field in required_fields if field not in data]
+#         if missing_fields:
+#             return jsonify({'message': f'Error: Missing fields - {", ".join(missing_fields)}'}), 400
+        
+#         # Extract data
+#         member_id = data['member_id']
+#         vin = data['vin']
+#         bid_value = data['bid_value']
+#         bid_status = 'Processing'
+        
+#         # Create a new bid entry
+#         new_bid = Bids(
+#             memberID=member_id,
+#             VIN_carID=vin,
+#             bidValue=bid_value,
+#             bidStatus=bid_status,
+#             bidTimestamp=datetime.now()
+#         )
+
+#         db.session.add(new_bid)
+#         db.session.commit()
+#         return jsonify({'message': 'Bid successfully inserted.'}), 201
+#     except Exception as e:
+#         # Rollback the transaction in case of an error
+#         db.session.rollback()
+#         return jsonify({'message': f'Error: {str(e)}'}), 500
