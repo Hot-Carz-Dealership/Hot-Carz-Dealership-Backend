1a2241f0818c3eb7bc85eabf6dd8321c6b7c82e8
Antonio Arce <antoarcejr@gmail.com>
2024-05-06 02:10:25 -0400

    RAILWAYS 🚂🚂🚂

--- config.py
+++ config.py
@@ -15,15 +15,15 @@ class Config:
     # SECRET_KEY = os.getenv("SECRET_KEY")
     if os.getenv('FLASK_ENV') == 'testing':
         # uncomment for local dev | comment for hosting
-        SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{os.getenv('SECRET_KEY')}@localhost/dealership_testing"
+        # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{os.getenv('SECRET_KEY')}@localhost/dealership_testing"
 
         # comment for local dev
-        # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:UUNaDUpaHzvdMkfieWerXOpyTTyHaxlI@viaduct.proxy.rlwy.net:39073/dealership_testing"
+        SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:UUNaDUpaHzvdMkfieWerXOpyTTyHaxlI@viaduct.proxy.rlwy.net:39073/dealership_testing"
     else:
         # uncomment for local dev | comment for hosting
-        SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{os.getenv('SECRET_KEY')}@localhost/dealership_backend"
+        # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{os.getenv('SECRET_KEY')}@localhost/dealership_backend"
 
         # comment for local dev | uncomment for hosting
-        # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:aGGeAzhlGdyhqpkesCDkjgcyKXHYXEuK@viaduct.proxy.rlwy.net:20836/dealership_backend"
+        SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:aGGeAzhlGdyhqpkesCDkjgcyKXHYXEuK@viaduct.proxy.rlwy.net:20836/dealership_backend"
     os.environ['FLASK_ENV'] = 'development'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
